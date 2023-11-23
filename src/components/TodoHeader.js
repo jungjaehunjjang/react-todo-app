67d4822b564596ec2ce46d00124d4f27c900389d
jaehun <josh9269@naver.com>
2023-11-23 16:36:44 +0900

    #6.  할 일 체크 기능,시간 표시 완료

--- src/components/TodoHeader.js
+++ src/components/TodoHeader.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './scss/TodoHeader.scss';
 
-const TodoHeader = () => {
+const TodoHeader = ({ count }) => {
+  const today = new Date();
+
+  const dateString = today.toLocaleDateString('ko-KR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
+  const datName = today.toLocaleDateString('ko-Kr', { weekday: 'long' });
+
   return (
     <header>
-      <h1>2023년 11월 23일</h1>
-      <div className='day'>목요일</div>
-      <div className='tasks-left'>할 일 x개 남음</div>
+      <h1>{dateString}</h1>
+      <div className='day'>{datName}</div>
+      <div className='tasks-left'>할 일 {count()}개 남음</div>
     </header>
   );
 };
